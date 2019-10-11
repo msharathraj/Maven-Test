b5011f92bacf417431b29876401f116ba80c8284
Sharath Raj Macharla <smacharla@intralinks.com>
2019-10-12 00:19:13 +0530

    Added new files for first checkin test repoowner

--- Sample.groovy
+++ Sample.groovy
@@ -1,41 +1,14 @@
-gitCreds = 'git'
-
-organizationFolder('Test-Jobs') {
-    description('This contains branch source jobs for Bitbucket')
-    displayName('Test Containers')
-    triggers {
-        periodic(86400)
+multibranchPipelineJob('example') {
+    branchSources {
+        git {
+            remote('https://github.com/msharathraj/SampleTest.git')
+            //credentialsId('github-ci')
+            includes('JENKINS-*')
+        }
     }
-    organizations {
-	
-		github( "msharathraj/SampleTest",  "https",  "github.com")
-    }
-	
-    configure { node ->
-        // node represents <jenkins.branch.OrganizationFolder>
-        def traits = node / 'navigators' / 'com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMNavigator' / 'traits'
-        traits << 'com.cloudbees.jenkins.plugins.bitbucket.BranchDiscoveryTrait' {
-            strategyId(3) // detect all branches
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
         }
     }
 }
-
-listView('Git-Flow-Jobs') {
-
-	description("All Git Flow Jobs.")
-    filterBuildQueue(true)
-    filterExecutors(true)
-    jobs {
-        regex('.*-Git')
-    }
-    columns {
-        status()
-     	weather()
-        name()
-        lastSuccess()
-        lastFailure()
-        lastDuration()
-        buildButton()
-    }
-}
-
