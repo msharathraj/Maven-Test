59e86b68b14db81b83a7fb8af4ce41c75fe994a5
Sharath Raj Macharla <smacharla@intralinks.com>
2019-10-11 23:45:39 +0530

    Added new files for first checkin test

--- Sample.groovy
+++ Sample.groovy
@@ -1,5 +1,33 @@
 gitCreds = 'git'
 
+organizationFolder('Canary-Containers') {
+    description('This contains branch source jobs for Bitbucket')
+    displayName('Canary Containers')
+    triggers {
+        periodic(86400)
+    }
+    organizations {
+	
+		git {
+          remote {
+			url('https://github.com/msharathraj/Maven-Test.git')
+            branch("develop")
+            extensions {
+                localBranch('develop')
+            }
+          }
+        }
+    }
+	
+    configure { node ->
+        // node represents <jenkins.branch.OrganizationFolder>
+        def traits = node / 'navigators' / 'com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMNavigator' / 'traits'
+        traits << 'com.cloudbees.jenkins.plugins.bitbucket.BranchDiscoveryTrait' {
+            strategyId(3) // detect all branches
+        }
+    }
+}
+
 listView('Git-Flow-Jobs') {
 	description("All Git Flow Jobs.")
     filterBuildQueue(true)
@@ -17,60 +45,4 @@ listView('Git-Flow-Jobs') {
         buildButton()
     }
 }
-
-
-multibranchPipelineJob("Test") {
-    // configure the branch / PR sources
-    branchSources {
-      branchSource {
-        source {
-          git {
-            //credentialsId("top-secret-1234-some-guid")
-            //repoOwner("${bitbucket_project.toUpperCase()}")
-            //repository("${bitbucket_repo}")
-            remote("https://github.com/msharathraj/Maven-Test.git")
-            traits {
-              headWildcardFilter {
-                includes("master release/* feature/* bugfix/*")
-                excludes("")
-              }
-            }
-          }
-        }
-        strategy {
-          defaultBranchPropertyStrategy {
-            props {
-              // keep only the last 10 builds
-              buildRetentionBranchProperty {
-                buildDiscarder {
-                  
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    // discover Branches (workaround due to JENKINS-46202) -
-    configure {
-      def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
-      traits << 'com.cloudbees.jenkins.plugins.bitbucket.BranchDiscoveryTrait' {
-        strategyId(3) // detect all branches
-      }
-    }
-
-    // check every minute for scm changes as well as new / deleted branches
-    triggers {
-      periodic(1)
-    }
-    // don't keep build jobs for deleted branches
-    orphanedItemStrategy {
-      discardOldItems {
-        numToKeep(-1)
-      }
-    }
-  }
-
-
-
 
