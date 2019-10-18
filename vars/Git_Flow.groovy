964834b20b32a6e2694585095d96913b2d92b321
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-19 00:32:55 +0530

    Update Git_Flow.groovy

--- vars/Git_Flow.groovy
+++ vars/Git_Flow.groovy
@@ -1,13 +1,15 @@
 def call(String gitRepo, String gitProj) {
-	
+	node{
 	stage('Merge-Release'){
 		bat "echo passed lib release"
 	}
-	
+	}
 }
 def artifactReleaseProcess(){
+node{
+	
 	stage('Merge-Release-lib'){
 		bat "echo passed lib release"
 	}	
-
+}
 }
