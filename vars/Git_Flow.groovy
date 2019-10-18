6d54f9a62da90bec3434aa41b36ed77e9ee6ec05
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-18 18:08:04 +0530

    Update Git_Flow.groovy

--- vars/Git_Flow.groovy
+++ vars/Git_Flow.groovy
@@ -1,4 +1,23 @@
-def call(String gitRepo, String gitProj) {
-	
-	println "Test"
+def call() {
+	node {
+		pipeline {
+			git {
+				remote {
+					url("https://github.com/msharathraj/Maven-Test.git")
+					branch('master')
+					extensions {
+					localBranch('master')
+				}
+			  }
+			}
+		stages {
+			release =  getReleasedVersion()
+			stage('Merge-Release'){
+				bat "echo passed lib"
+			}
+		}	
+		
+		}
+        
+    }
 }
