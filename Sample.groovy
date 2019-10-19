5856d6cfabd6d64f711cfa14f354e73bf9308cab
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-19 11:48:52 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -1,10 +1,5 @@
 job('Test-Artifactory'){
 	
-	def server = Artifactory.server 'jenkins-artifactory-server' , username: 'admin', password: 'password'
-	def rtMaven = Artifactory.newMavenBuild()
-	rtMaven.resolver server: server, releaseRepo: 'sample-repo', snapshotRepo: 'sample-repo-snapshot'
-	rtMaven.deployer server: server, releaseRepo: 'sample-repo-local', snapshotRepo: 'sample-repo-local'
-	rtMaven.deployer.deployArtifacts = true
 	
 		parameters {
 			choiceParam('SOURCE_BRANCH', ['Master', 'Develop'], 'Source branch from code is merged to Destination')
@@ -12,7 +7,34 @@ job('Test-Artifactory'){
 			choiceParam('TAG_REQUIRED', ['Yes', 'No'], 'Do you require a tag creation')
 		}
 		println "asd"
+		scm {
+			git {
+				remote {
+					// This is used for getting the code checkout from the given repository
+					url("https://github.com/cameronmcnz/rock-paper-scissors.git")
+					branch('master')
+					extensions {
+					localBranch('master')
+				}
+			  }
+			}
+		}
+		stages {
+			stage('Release'){
+				arti()
+				rtMaven.tool = 'MAVEN'
+				def buildInfo = rtMaven.run goals: 'clean install'
+				server.publishBuildInfo buildInfo
+			}
+		}
 	
 }
-
+def arti(){
+	
+	def server = Artifactory.server 'jenkins-artifactory-server' , username: 'admin', password: 'password'
+	def rtMaven = Artifactory.newMavenBuild()
+	rtMaven.resolver server: server, releaseRepo: 'sample-repo', snapshotRepo: 'sample-repo-snapshot'
+	rtMaven.deployer server: server, releaseRepo: 'sample-repo-local', snapshotRepo: 'sample-repo-local'
+	rtMaven.deployer.deployArtifacts = 'true'
+}
 	
