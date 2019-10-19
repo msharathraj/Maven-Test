763090f171a8e469b1ba2e1c0bcf7ed8658acb86
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-19 10:29:19 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -11,25 +11,7 @@ job('Test-Artifactory'){
 			choiceParam('DESTINATION_BRANCH', ['Master', 'Release'], 'Destination branch where the code should be merged')
 			choiceParam('TAG_REQUIRED', ['Yes', 'No'], 'Do you require a tag creation')
 		}
-		scm {
-			git {
-				remote {
-					// This is used for getting the code checkout from the given repository
-					url("https://github.com/cameronmcnz/rock-paper-scissors.git")
-					branch('master')
-					extensions {
-					localBranch('master')
-				}
-			  }
-			}
-		}
-		stages {
-			stage('Release'){
-				rtMaven.tool = 'MAVEN'
-				def buildInfo = rtMaven.run goals: 'clean install'
-				server.publishBuildInfo buildInfo
-			}
-		}
+		println "asd"
 	
 }
 
