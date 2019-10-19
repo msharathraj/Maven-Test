98c4ff21f9aa1f147a7efd0c5caf1e05ec86b2ef
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-19 16:19:23 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -1,40 +1,20 @@
-import hudson.maven.*;
-import hudson.tasks.*;
-
-freeStyleJob('Test-Artifactory'){
-	
-	def server = Artifactory.server 'jenkins-artifactory-server' , username: 'admin', password: 'password'
-	def rtMaven = Artifactory.newMavenBuild()
-	rtMaven.resolver server: server, releaseRepo: 'sample-repo', snapshotRepo: 'sample-repo-snapshot'
-	rtMaven.deployer server: server, releaseRepo: 'sample-repo-local', snapshotRepo: 'sample-repo-local'
-	rtMaven.deployer.deployArtifacts = true
-	pipeline{
-		parameters {
-			choiceParam('SOURCE_BRANCH', ['Master', 'Develop'], 'Source branch from code is merged to Destination')
-			choiceParam('DESTINATION_BRANCH', ['Master', 'Release'], 'Destination branch where the code should be merged')
-			choiceParam('TAG_REQUIRED', ['Yes', 'No'], 'Do you require a tag creation')
-		}
-		scm {
-			git {
-				remote {
-					// This is used for getting the code checkout from the given repository
-					url("https://github.com/cameronmcnz/rock-paper-scissors.git")
-					branch('master')
-					extensions {
-					localBranch('master')
-				}
-			  }
-			}
-		}
-		stages {
-			stage('Release'){
-				rtMaven.tool = 'MAVEN'
-				def buildInfo = rtMaven.run goals: 'clean install'
-				server.publishBuildInfo buildInfo
-			}
-		}
-	}	
-	
+job(){
+node {
+  withMaven(maven:'M3') {
+    stage('Checkout') {
+      git url: 'https://github.com/piomin/sample-spring-cloud-contract-ci.git', credentialsId: 'piomin-github', branch: 'master'
+    }
+    stage('Publish') {
+      def server = Artifactory.server 'artifactory'
+      def rtMaven = Artifactory.newMavenBuild()
+      rtMaven.tool = 'M3'
+      rtMaven.resolver server: server, releaseRepo: 'libs-release', snapshotRepo: 'libs-snapshot'
+      rtMaven.deployer server: server, releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local'
+      rtMaven.deployer.artifactDeploymentPatterns.addInclude("*stubs*")
+      def buildInfo = rtMaven.run pom: 'person-service/pom.xml', goals: 'clean install'
+      rtMaven.deployer.deployArtifacts buildInfo
+      server.publishBuildInfo buildInfo
+    }
+  }
 }
-
-	
+}
