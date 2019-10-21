b27dea06f73d6ade5130864ad1f2f1f56ebd0d04
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-21 21:55:43 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -1,23 +1,38 @@
+#!/usr/bin/env groovy
+
+//@Library('Artifactory-Publish') _
+
 pipeline
-{
-	agent any
-	parameters {
-		choiceParam('SOURCE_BRANCH', ['Master', 'Develop'], 'Source branch from code is merged to Destination')
-		choiceParam('DESTINATION_BRANCH', ['Master', 'Release'], 'Destination branch where the code should be merged')
-		stringParam('RELEASE_BRANCH', '')
-	}
-	    stage('Build') { 
-            steps{	
+ {
+		parameters {
+			choiceParam('SOURCE_BRANCH', ['Master', 'Develop'], 'Source branch from code is merged to Destination')
+			choiceParam('DESTINATION_BRANCH', ['Master', 'Release'], 'Destination branch where the code should be merged')
+			stringParam('RELEASE_BRANCH', '')
+		}
+		
+		stages {
+		stage('checkout')
+		{
+			steps{	
 				git  url : 'https://stash.intralinks.com/scm/qe/qe-pom.git', credentialsId: 'git'
 			}	
-        }
-        stage('Test'){
-            steps {
-                println 'test'
-            }
-        }
-        stage('Deploy') {
-            
-        }
-    
-}
+		}	
+			stage('Develop-Merge-Release'){
+			steps{
+			// This stage is going to merge the code from develop to release branch and runs maven to check build
+				println 'ads'
+				}
+			}
+			stage('Release-Merge-Master'){
+			// This stage is going to merge the code from develop to release branch and runs maven to check build
+			steps{
+					printlin 'merge'
+				
+				}
+			}
+			
+			stage('Release-to-artifactory'){
+				//Git_Flow()
+			}
+		}
+	}
