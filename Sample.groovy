30c36ab73458b4ee5105114d8dc0387d5276130a
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-22 00:42:34 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -1,14 +1,43 @@
+@Library('Maven-Test@test') _
 node {  
-    stage('Build') { 
-        // 
-        steps{
-        bat "echo build"
-        }
-    }
-    stage('Test') { 
-        // 
-    }
-    stage('Deploy') { 
-        // 
-    }
+    parameters {
+		choiceParam('SOURCE_BRANCH', ['Master', 'Develop'], 'Source branch from code is merged to Destination')
+		choiceParam('DESTINATION_BRANCH', ['Master', 'Release'], 'Destination branch where the code should be merged')
+		stringParam('RELEASE_BRANCH', '')
+	}
+		
+	stage('checkout')
+	{
+		scm {
+            git {
+                remote {
+                    url('https://github.com/cameronmcnz/rock-paper-scissors.git')
+                }
+                branch("master")
+            }
+        }	
+	}	
+	stage('Develop-Merge-Release'){
+		steps{
+		// This stage is going to merge the code from develop to release branch and runs maven to check build
+			bat('''
+			git branch 
+			echo "Merging the code from Develop Branch to Release Branch"
+            mvn install
+			''')
+		}
+	}
+	stage('Release-Merge-Master'){
+		// This stage is going to merge the code from develop to release branch and runs maven to check build
+			bat('''
+			git branch 
+			echo "Merging the code from Master Branch to Release Branch"
+            mvn install
+			
+			''')
+		
+	}
+	stage('Release-to-artifactory'){
+		Git_Flow()
+	}
 }
