3d58e06a0e96f165ad8419683af4f5f18b2e66d3
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-21 23:10:31 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -1,7 +1,27 @@
-pipelineJob('dsad') {
-    deliveryPipelineConfiguration('qa')
-    steps{
-        deliveryPipelineConfiguration('qa1')
-        
+node {
+    // Get Artifactory server instance, defined in the Artifactory Plugin administration page.
+    def server = Artifactory.server "jenkins-artifactory-server"
+    // Create an Artifactory Maven instance.
+    def rtMaven = Artifactory.newMavenBuild()
+    def buildInfo
+
+    stage('Clone sources') {
+        git url: 'https://github.com/jfrogdev/project-examples.git'
+    }
+
+    stage('Artifactory configuration') {
+        // Tool name from Jenkins configuration
+        rtMaven.tool = "MAVEN"
+        // Set Artifactory repositories for dependencies resolution and artifacts deployment.
+        rtMaven.deployer releaseRepo:'libs-release-local', snapshotRepo:'libs-snapshot-local', server: server
+        rtMaven.resolver releaseRepo:'libs-release', snapshotRepo:'libs-snapshot', server: server
+    }
+
+    stage('Maven build') {
+        buildInfo = rtMaven.run pom: 'maven-example/pom.xml', goals: 'clean install'
+    }
+
+    stage('Publish build info') {
+        server.publishBuildInfo buildInfo
     }
 }
