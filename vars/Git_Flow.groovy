dce13c148921efcd9cc5ed230bc5bdc8f52eb961
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-21 14:39:13 +0530

    Update Git_Flow.groovy

--- vars/Git_Flow.groovy
+++ vars/Git_Flow.groovy
@@ -2,32 +2,38 @@ def server
 def buildInfo
 def rtMaven
     
-def call(){
-	
+def call(String depReleaseRepo,String depSnapshot,String resReleaseRepo,String resSnapshot){
+		
 	rtServer (id: "jenkins-artifactory-server",  url: 'http://localhost:8081/artifactory',
-                    username: 'admin',
-					password: 'password'
-                )
-                rtMavenDeployer (
-                    id: "deployer-unique-id",
-                    serverId: "jenkins-artifactory-server",
-                    releaseRepo: "sample-repo",
-                    snapshotRepo: "sample-repo",
-                    deployArtifacts:"true"
-                )
-                rtMavenResolver (
-                    id: "resolver-unique-id",
-                    serverId: "jenkins-artifactory-server",
-                    releaseRepo: "sample-repo",
-                    snapshotRepo: "sample-repo"
-                )
-				rtUpload(
-					serverId: "jenkins-artifactory-server",
-					pattern: "*roshambo*.jar",
-					target: "/sample-repo/"
-				)
-				rtPublishBuildInfo(
-					serverId: "jenkins-artifactory-server",
-				)	
+				username: 'admin', password: 'password'
+             )
+    	rtMavenDeployer (
+	    id: "deployer-unique-id",
+            serverId: "jenkins-artifactory-server",
+            releaseRepo: "sample-repo",
+            snapshotRepo: "sample-repo",
+            deployArtifacts:"true"
+            )
+        rtMavenResolver (
+            id: "resolver-unique-id",
+            serverId: "jenkins-artifactory-server",D
+            releaseRepo: "sample-repo",
+            snapshotRepo: "sample-repo"
+           )
+		   
+	rtMavenRun (
+		tool: 'MAVEN', 
+		pom: 'pom.xml',
+		goals: 'clean install',
+		deployerId: "deployer-unique-id",
+		resolverId: "resolver-unique-id"
+		)		
+	rtUpload(
+		serverId: "jenkins-artifactory-server",
+     		target: "/sample-repo/"
+		)
+	rtPublishBuildInfo(
+		serverId: "jenkins-artifactory-server",
+	)
 	
 }
