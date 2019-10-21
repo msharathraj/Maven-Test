ae40d59a8a16beb0dbad51fdc825eb65ebff8d8e
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-21 21:52:40 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -1,16 +1,12 @@
 pipeline
- {
-		agent any
-		parameters {
-			choiceParam('SOURCE_BRANCH', ['Master', 'Develop'], 'Source branch from code is merged to Destination')
-			choiceParam('DESTINATION_BRANCH', ['Master', 'Release'], 'Destination branch where the code should be merged')
-			stringParam('RELEASE_BRANCH', '')
-		}
-		
-		
-			
-		stages {
-        stage('Build') { 
+{
+	agent any
+	parameters {
+		choiceParam('SOURCE_BRANCH', ['Master', 'Develop'], 'Source branch from code is merged to Destination')
+		choiceParam('DESTINATION_BRANCH', ['Master', 'Release'], 'Destination branch where the code should be merged')
+		stringParam('RELEASE_BRANCH', '')
+	}
+	    stage('Build') { 
             steps{	
 				git  url : 'https://stash.intralinks.com/scm/qe/qe-pom.git', credentialsId: 'git'
 			}	
@@ -23,7 +19,5 @@ pipeline
         stage('Deploy') {
             
         }
-    }
-		
-		
-	}
+    
+}
