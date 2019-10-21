568d30db6ac7a26dca767cffc5e9228619434218
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-22 00:07:21 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -2,7 +2,7 @@
 
 @Library('Maven-Test@test') _
 
-pipeline{
+node('master'){
 	parameters {
 		choiceParam('SOURCE_BRANCH', ['Master', 'Develop'], 'Source branch from code is merged to Destination')
 		choiceParam('DESTINATION_BRANCH', ['Master', 'Release'], 'Destination branch where the code should be merged')
