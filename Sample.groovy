3aad6b75d0e361b1fe8a0bcf00b750ae60218a13
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-21 22:35:41 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -1,39 +1,11 @@
-#!/usr/bin/env groovy
-
-//@Library('Artifactory-Publish') _
-
-job('Testing')
- {		
-	 node{
-		parameters {
-			choiceParam('SOURCE_BRANCH', ['Master', 'Develop'], 'Source branch from code is merged to Destination')
-			choiceParam('DESTINATION_BRANCH', ['Master', 'Release'], 'Destination branch where the code should be merged')
-			stringParam('RELEASE_BRANCH', '')
-		}
-		
-		stage('checkout')
-		{
-			steps{	
-				git  url : 'https://stash.intralinks.com/scm/qe/qe-pom.git', credentialsId: 'git'
-			}	
-		}	
-			stage('Develop-Merge-Release'){
-			steps{
-			// This stage is going to merge the code from develop to release branch and runs maven to check build
-				println 'ads'
-				}
-			}
-			stage('Release-Merge-Master'){
-			// This stage is going to merge the code from develop to release branch and runs maven to check build
-			steps{
-					printlin 'merge'
-				
-				}
-			}
+def test
+pipeline{
+	
+	stages{
+		stage('sadd'){
 			
-			stage('Release-to-artifactory'){
-				//Git_Flow()
-			}
-		
+			println 'asd'
+		}
 	}
- }
+	
+}
