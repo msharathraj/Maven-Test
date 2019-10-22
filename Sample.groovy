80fa072b00ce846267740936971d6864282362f3
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-22 17:48:40 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -1,11 +1,12 @@
 @Library('Maven-Test@test') _
 pipeline {  
-    	
+    	agent any
+	stages{
 	stage('checkout')
 	{
 	    git url: 'https://github.com/cameronmcnz/rock-paper-scissors.git' 
 		Git_Flow()
         	
 	}	
-	
+	}
 }
