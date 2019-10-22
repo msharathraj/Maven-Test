31844540ae10201a917b97dd6fc93ca16ef58131
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-22 17:47:57 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -1,11 +1,6 @@
 @Library('Maven-Test@test') _
 pipeline {  
-    parameters {
-		choiceParam('SOURCE_BRANCH', ['Master', 'Develop'], 'Source branch from code is merged to Destination')
-		choiceParam('DESTINATION_BRANCH', ['Master', 'Release'], 'Destination branch where the code should be merged')
-		stringParam('RELEASE_BRANCH', '')
-	}
-		
+    	
 	stage('checkout')
 	{
 	    git url: 'https://github.com/cameronmcnz/rock-paper-scissors.git' 
