8b14d50cd500b9c5551bca14299e3a338d960b0a
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-23 14:04:30 +0530

    Update Git_Flow.groovy

--- vars/Git_Flow.groovy
+++ vars/Git_Flow.groovy
@@ -1,19 +1,31 @@
 def server
 def buildInfo
 def rtMaven
+def descriptor
+
     
 def call(){
 	
-	server = Artifactory.newServer url: 'http://localhost:8081/artifactory', username: 'admin', password: 'password'
-	rtMaven = Artifactory.newMavenBuild()
-	rtMaven.resolver server: server, releaseRepo: 'sample-repo', snapshotRepo: 'sample-repo'
-	rtMaven.deployer server: server, releaseRepo: 'sample-repo', snapshotRepo: 'sample-repo'
-	rtMaven.tool = 'MAVEN'
-	
-	def buildInfo = rtMaven.run pom: 'pom.xml', goals: 'clean install'
-	rtMaven.run pom: 'maven-example/pom.xml', goals: 'clean install', buildInfo: existingBuildInfo
-	
+	def server = Artifactory.server 'jenkins-artifactory-server'
+	def descriptor = Artifactory.mavenDescriptor()
+	descriptor.version = '1.2.0'
+	descriptor.transform()
+	def rtMaven = Artifactory.newMavenBuild()
+        rtMaven.tool = 'MAVEN'
+        rtMaven.deployer releaseRepo: 'sample-repo', snapshotRepo: 'sample-repo-snapshot', server: server
+
+        def buildInfo = Artifactory.newBuildInfo()
+        rtMaven.run pom: 'pom.xml', goals: 'install package', buildInfo: buildInfo
+        def uploadSpec = """{
+	  "files": [
+		{	
+		  "pattern": "1.0*.jar",
+		  "target": "sample-repo/"
+		}
+	 ]
+	}"""
+	server.upload spec: uploadSpec
+        rtMaven.deployer.deployArtifacts = true // Disable artifacts deployment during Maven run
 	rtMaven.deployer.deployArtifacts buildInfo
-	server.publishBuildInfo buildInfo
-	
+	server.publishBuildInfo buildInfo	
 }
