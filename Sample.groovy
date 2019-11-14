1afab5d8f65da3b31ffb799c6824559d09457d1e
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-11-14 16:18:21 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -1,13 +1,53 @@
 @Library('Maven-Test@test') _
-pipeline {  
-    	agent any
-	stages{
-	stage('checkout')
-	{
-		steps{
-	    git url: 'https://github.com/cameronmcnz/rock-paper-scissors.git' 
-		Git_Flow()
-		}
-	}	
-	}
-}
+
+job("Releae-Project") {
+        description("This is release process job")
+        label('master')
+        keepDependencies(false)
+        parameters {
+            stringParam("PROJECT", "qe", "The project key specified for this project in GIT.")
+            stringParam("REPOSITORY", "qe-pom", "The GIT branch to scan.")
+            stringParam("SOURCE_BRANCH", "qe", "The GIT branch to checkout.")
+        }
+	scm {
+            git {
+                remote {
+                    github("https://github.com/msharathraj/Maven-Test.git")
+                }
+            }
+        }	
+        
+        disabled(false)
+        concurrentBuild(false)
+        steps {
+            shell("echo git branch")
+        }
+        
+        publishers {
+            extendedEmail {
+                triggers {
+                    always {
+                        subject("\$PROJECT_DEFAULT_SUBJECT")
+                        content("\$PROJECT_DEFAULT_CONTENT")
+                        attachmentPatterns()
+                        attachBuildLog(false)
+                        compressBuildLog(false)
+                        contentType("project")
+                        sendTo {
+                          recipientList()
+                        }
+                    }
+                }
+                contentType("default")
+                defaultSubject("\$DEFAULT_SUBJECT")
+                defaultContent("\$DEFAULT_CONTENT")
+                attachmentPatterns()
+                preSendScript("\$DEFAULT_PRESEND_SCRIPT")
+                attachBuildLog(false)
+                compressBuildLog(false)
+                replyToList()
+                saveToWorkspace(false)
+                disabled(false)
+            }
+        }
+    }
